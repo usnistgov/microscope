23c65ae3c61d6bc18b3ae00b15fa44a5871a7365
Joe Fowler <drjoefowler@gmail.com>
2017-12-05 08:59:57 -0700

    Different baseline level per channel in demo pulses.

--- demo/demo_pulses.py
+++ demo/demo_pulses.py
@@ -24,13 +24,13 @@ socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:%s" % port)
 
 t = np.linspace(-256, 767, 1024)
-pulse = 30000*(np.exp(-t/200.) - np.exp(-t/40)) + 1000
-pulse[t<0] = 1000
+pulse = 30000*(np.exp(-t/200.) - np.exp(-t/40)) + 0
+pulse[t<0] = 0
 messagedata = np.asarray(pulse, dtype=np.uint16)
 
 while True:
-    channel = random.randrange(1,8)
-    message = message_definition.DastardPulse(channel, 256, messagedata)
-    print "chan %d data: %s message length %d" % (channel, messagedata, len(message.pack()))
+    channel = random.randrange(0,8)
+    message = message_definition.DastardPulse(channel, 256, 1000*channel+messagedata)
+    print "chan %d data: %s message length %d" % (channel, 1000*channel+messagedata, len(message.pack()))
     socket.send(message.pack())
-    time.sleep(1)
+    time.sleep(0.3)
