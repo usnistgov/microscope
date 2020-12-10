usnistgov/microscope

commit cc367c9fd17e5f7fe2f3e1970ef5dc169befd16e

plotWindow stores maps between channel name and index

plotwindow.cpp
@@ -76,6 +76,7 @@ plotWindow::plotWindow(zmq::context_t *context_in, options *opt, QWidget *parent
         delete chansocket;
         chansocket = nullptr;
     }
+    buildNameIndexTables(opt);
 
     setWindowFlags(Qt::Window);
     setAttribute(Qt::WA_DeleteOnClose); // important!
@@ -247,6 +248,35 @@ plotWindow::plotWindow(zmq::context_t *context_in, options *opt, QWidget *parent
         ui->actionY_axis_raw_units->trigger();
     else
         ui->actionY_axis_phys_units->trigger();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Fill in     QVector<int> channelNumbers and QMap<int, int> channelNum2Index
+///
+/// These map channel index to number and number to index, respectively.
+///
+void plotWindow::buildNameIndexTables(const options *opt) {
+    int index=0;
+    foreach(channelGroup cg, opt->chanGroups) {
+        for (int cnum=cg.firstchan; cnum<cg.firstchan+cg.nchan; cnum++) {
+            QString name = QString("Ch %1").arg(cnum);
+            channelNames.append(name);
+            channelName2Index[name] = index;
+            index++;
+            if (hasErr) {
+                QString name = QString("Err %1").arg(cnum);
+                channelNames.append(name);
+                channelName2Index[name] = index;
+                index++;
+            }
+        }
+    }
+    std::cout << "Debug:\n";
+    for (int idx=0; idx<channelNames.size(); idx++) {
+        std::cout << "Channel Index " << idx << "-> " << channelNames[idx].toStdString() <<
+            "-> Index " << channelName2Index[channelNames[idx]] << std::endl;
+    }
 }
 
 

plotwindow.h
@@ -15,6 +15,7 @@
 #include <QActionGroup>
 #include <QCheckBox>
 #include <QColor>
+#include <QMap>
 #include <QSettings>
 #include <QString>
 #include <QVector>
@@ -133,6 +134,10 @@ private:
     zmq::context_t *zmqcontext;
     zmq::socket_t *chansocket;
 
+    QVector<QString> channelNames;        ///< The channel names in this source, in order
+    QMap<QString, int> channelName2Index; ///< Map from channel number to channel index
+
+    void buildNameIndexTables(const options *opt);
     void startRefresh();
     void rescalePlots(QCPGraph *);
     void closeEvent(QCloseEvent *);

commit d5e1b93caa5298008349725a7853bd5e0519595a

Fix error/FB ordering problem
Introduced a bug in cc367c9. Fixes #38.

plotwindow.cpp
@@ -260,16 +260,16 @@ void plotWindow::buildNameIndexTables(const options *opt) {
     int index=0;
     foreach(channelGroup cg, opt->chanGroups) {
         for (int cnum=cg.firstchan; cnum<cg.firstchan+cg.nchan; cnum++) {
-            QString name = QString("Ch %1").arg(cnum);
-            channelNames.append(name);
-            channelName2Index[name] = index;
-            index++;
             if (hasErr) {
                 QString name = QString("Err %1").arg(cnum);
                 channelNames.append(name);
                 channelName2Index[name] = index;
                 index++;
             }
+            QString name = QString("Ch %1").arg(cnum);
+            channelNames.append(name);
+            channelName2Index[name] = index;
+            index++;
         }
     }
     // std::cout << "Debug:\n";