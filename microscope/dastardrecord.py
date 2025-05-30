5bd3da69de5cf03040a54d192f26bc0893aea92f
Joe Fowler <drjoefowler@gmail.com>
2025-05-30 07:46:27 -0600

    More FFT comments and docstrings

--- microscope/dastardrecord.py
+++ microscope/dastardrecord.py
@@ -5,10 +5,56 @@ from collections import deque
 from dataclasses import dataclass, replace
 from functools import cache
 
+__info__ = """
+Some notes on estimating power spectral density (PSD) using the discrete Fourier Transform (DFT).
+
+Because we are calculating the power spectral density, we should do some
+windowing to prevent excessive leakage. basically, multiply each number by some
+scaling factor that is ~0 at the edges and ~unity in the middle. we do this b/c
+we see a snapshot of the entire data stream and there is considerable spectral
+leakage from one bin to the next. because at the left and right, we have only
+the tailing or leading bins to look at, we do not get a complete picture of
+where they came from. so, instead, we hardly look at those points, where the
+information is incomplete and look more at the middle points where we know both
+trailing and leading edges. windowing methods accomplish this. see numerical
+recipes p553
+
+Note about the normalization for delta t fftw gives us volts per root
+sample. We want volts per root hz, so we need to have a conversion factor
+that does this. Given Randy's code, it seems this factor should be root
+(2*deltaT).
+
+Note from Randy D, found in xcaldaqclient, then MATTER, then the C++ microscope:
+Be sure that you are averaging the SQUARED signals (PSD in V2/Hz) and then displaying
+the sqrt of that average.  If you, instead, just average the V/sqrt(Hz) signal,
+you will be off by an insidiously small gain factor, something like 8%,
+depending on the f binning!  This was a hard-won bug removal for me.
+"""
+
+# Memoize (`@cache`) these two functions (DFT window and frequency bin values) because we expect
+# to use with the same arguments over and over.
+
 
 @cache
 def windowFunction(N: int) -> np.ndarray:
-    return np.hanning(N)
+    """windowFunction(N)
+
+    Parameters
+    ----------
+    N : int
+        Length of the data being DFTed
+
+    Returns
+    -------
+    np.ndarray
+        An appropriate window function, normalized.
+
+    When multiplying data by a window function, the amount of power in the signal can be
+    changed.  To compensate for this, the mean(window[i]^2) is scaled to 1.
+    """
+    window = np.hanning(N)
+    window *= 1/np.sqrt((window**2).mean())
+    return window
 
 
 @cache
@@ -58,6 +104,19 @@ class DastardRecord:
 
 
 def meanDastardRecord(buffer: deque[DastardRecord]) -> DastardRecord:
+    """meanDastardRecord - Compute a DastardRecord equal to the mean of the records in `buffer`.
+
+    Parameters
+    ----------
+    buffer : deque[DastardRecord]
+        A history of the last N DastardRecords on this channel.
+
+    Returns
+    -------
+    DastardRecord
+        A synthetic DastardRecord whose pulse payload equals the mean of those of all the
+        records in the deque. The mean record will have floating-point values.
+    """
     n = len(buffer)
     assert n > 0
 
@@ -67,4 +126,4 @@ def meanDastardRecord(buffer: deque[DastardRecord]) -> DastardRecord:
     raw = np.zeros_like(buffer[0].record, dtype=float)
     for dr in buffer:
         raw += dr.record
-    return replace(buffer[0], record=raw / n)
+    return replace(buffer[-1], record=raw / n)
